276e492ebe146738fb772549524957b4ada7bca8
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-27 14:57:21 -0700

    selinux: Avoid dynamic memory allocation for temporary scontext buffers
    Common sizes for the scontext buffers are small enough to fit on the stack,
    saving two dynamic memory allocations from this hot path.
    
    Change-Id: I9bace557c9ed55cee5c27a29cde4d2c897242c96
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -1586,6 +1586,10 @@ retry:
 	} else if (rc)
 		goto out_unlock;
 	rc = sidtab_context_to_sid(sidtab, &context, sid);
+
+	if (context.str == str_onstack)
+		context.str = NULL;
+
 	if (rc == -ESTALE) {
 		rcu_read_unlock();
 		if (context.str) {
