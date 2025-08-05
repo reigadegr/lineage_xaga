4f2ff47a04f5f606421350fe7fa5a22046fa877e
brokestar233 <3765589194@qq.com>
2025-08-05 14:48:40 +0800

    zram: Refactor disksize calculation
    This commit refactors the zram disk size calculation logic to be more flexible and responsive to system memory.
    
    The change removes the hardcoded 16GB default disk size and introduces a new dynamic sizing mechanism. The disk size is now calculated based on the total system RAM, rounded up to the nearest gigabyte. This approach ensures that the zram device size is proportional to the system's physical memory, leading to more efficient memory utilization.
    
    Additionally, the patch removes the conditional compilation for the `_round_up` helper function, making it available for general use within the zram driver.
    
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1924,6 +1924,8 @@ static int zram_add(void)
 	struct zram *zram;
 	struct request_queue *queue;
 	int ret, device_id;
+	unsigned long total_mem = (u64)totalram_pages() << PAGE_SHIFT; // 总物理内存
+	u64 default_disksize = 2*total_mem;
 
 	zram = kzalloc(sizeof(struct zram), GFP_KERNEL);
 	if (!zram)
@@ -1964,6 +1966,36 @@ static int zram_add(void)
 
 	/* Actual capacity set using syfs (/sys/block/zram<id>/disksize */
 	set_capacity(zram->disk, 0);
+
+	down_write(&zram->init_lock);
+	if (!zram_meta_alloc(zram, default_disksize)) {
+		up_write(&zram->init_lock);
+		ret = -ENOMEM;
+		goto out_cleanup_disk;
+	}
+
+	for (u32 prio = 0; prio < ZRAM_MAX_COMPS; prio++) {
+		if (!zram->comp_algs[prio])
+			continue;
+
+		struct zcomp *comp = zcomp_create(zram->comp_algs[prio]);
+		if (IS_ERR(comp)) {
+			pr_err("Cannot initialise %s compressing backend\n",
+				zram->comp_algs[prio]);
+			zram_destroy_comps(zram); // 清理已创建的压缩器
+			zram_meta_free(zram, default_disksize); // 释放元数据
+			up_write(&zram->init_lock);
+			ret = PTR_ERR(comp);
+			goto out_cleanup_disk;
+		}
+		zram->comps[prio] = comp;
+		zram->num_active_comps++;
+	}
+
+	zram->disksize = default_disksize;
+	set_capacity_and_notify(zram->disk, zram->disksize >> SECTOR_SHIFT);
+	up_write(&zram->init_lock);
+
 	/* zram devices sort of resembles non-rotational disks */
 	blk_queue_flag_set(QUEUE_FLAG_NONROT, zram->disk->queue);
 	blk_queue_flag_clear(QUEUE_FLAG_ADD_RANDOM, zram->disk->queue);
@@ -1986,7 +2018,7 @@ static int zram_add(void)
 	 * size is identical with physical block size(PAGE_SIZE). But if it is
 	 * different, we will skip discarding some parts of logical blocks in
 	 * the part of the request range which isn't aligned to physical block
-	 * size.  So we can't ensure that all discarded logical blocks are
+	 * size.  So we can't ensure that all discarded logical blocks are
 	 * zeroed.
 	 */
 	if (ZRAM_LOGICAL_BLOCK_SIZE == PAGE_SIZE)
@@ -1998,7 +2030,7 @@ static int zram_add(void)
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
 
 	zram_debugfs_register(zram);
-	pr_info("Added device: %s\n", zram->disk->disk_name);
+	pr_info("Added device: %s with default size %llu bytes\n", zram->disk->disk_name, default_disksize);
 	return device_id;
 
 out_free_queue:
