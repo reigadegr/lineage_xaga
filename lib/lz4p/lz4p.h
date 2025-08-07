0dfeee8c67a7c478967cf226d8a117747f91b724
Pzqqt <821026875@qq.com>
2025-08-07 16:38:04 +0800

    lib: Import lz4p compression algorithm
    - From dijun-v-oss

---
+++ lib/lz4p/lz4p.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * XRING shrink slabd Feature
+ *
+ * Copyright (C) 2024, X-Ring technologies Inc., All rights reserved.
+ *
+ */
+
+#ifndef __LINUX_XRING_LZ4P_H
+#define __LINUX_XRING_LZ4P_H
+
+#ifdef CONFIG_XRING_ZRAM_LZ4P
+#include "lz4p_compress.h"
+#include "lz4p_decompress.h"
+#include "lz4p_constants.h"
+
+int lz4p_init(void);
+void lz4p_exit(void);
+#endif
+
+#endif /* __LINUX_XRING_LZ4P_H */
