5951745611b91378ee137dfa7e41a541f2a0bfb5
reigadegr <2722688642@qq.com>
2025-12-22 03:23:00 +0800

    slub: keep "random_seq" field unconditionally to preserve ABI
        The "random_seq" field is wrapped in #ifdef CONFIG_SLAB_FREELIST_RANDOM, so that the size of the structure changes when CONFIG_SLAB_FREELIST_RANDOM is disabled.
    
        This breaks the kernel module ABI: pre-built modules that were
        compiled against a kernel that had the option enabled contain
        hard-coded sizeof(struct kmem_cache) values and offset-of "random"
        assumptions.  After the option is turned off those modules can no
        longer be loaded, showing:
    
          kernel_fb: disagrees about version of symbol kmalloc_caches
          kernel_fb: disagrees about version of symbol kmalloc_trace
    
        Because we can not re-compile the affected modules, the only way to
        restore compatibility is to keep the field present in *all* kernels,
        independent of the config option.  The extra eight bytes are harmless
        and the code that actually uses the field is still guarded by
        CONFIG_SLAB_FREELIST_RANDOM, so no functional change is introduced
        when the option is disabled.
    
        Leave the field in struct kmem_cache unconditionally and remove the
        #ifdef/#endif pair.  This makes the ABI stable regardless of the
        config option and allows already-shipped modules to load again.
    
        Signed-off-by: reigadegr <2722688642@qq.com>

--- include/linux/slub_def.h
+++ include/linux/slub_def.h
@@ -119,9 +119,7 @@ struct kmem_cache {
 	unsigned int remote_node_defrag_ratio;
 #endif
 
-#ifdef CONFIG_SLAB_FREELIST_RANDOM
 	unsigned int *random_seq;
-#endif
 
 #ifdef CONFIG_KASAN
 	struct kasan_cache kasan_info;
