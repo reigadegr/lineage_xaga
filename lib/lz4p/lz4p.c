523cbc6aca15ecdae14307a686f41424a43c7d25
reigadegr <2722688642@qq.com>
2026-01-03 17:22:05 +0800

    Revert "lib: lz4p: Register lz4p algorithm by default"
    This reverts commit 1a15e894a026d365f98f5fe9315ab0dd97104f26.

--- lib/lz4p/lz4p.c
+++ lib/lz4p/lz4p.c
@@ -135,7 +135,7 @@ static struct scomp_alg scomp = {
 	}
 };
 
-static int lz4p_init(void)
+int lz4p_init(void)
 {
 	int ret;
 
@@ -155,15 +155,8 @@ static int lz4p_init(void)
 	return ret;
 }
 
-static void lz4p_exit(void)
+void lz4p_exit(void)
 {
 	crypto_unregister_alg(&alg_lz4);
 	crypto_unregister_scomp(&scomp);
 }
-
-subsys_initcall(lz4p_init);
-module_exit(lz4p_exit);
-
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("lz4p Compression Algorithm");
-MODULE_ALIAS_CRYPTO("lz4p");
